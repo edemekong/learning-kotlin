63657f1711953a79313ac2b220975afb01241282
Paul Edemekong <hello.paulvin@gmail.com>
2022-01-27 04:36:46 +0100

    first commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,31 @@
 
 fun main(args: Array<String>) {
+    val nums = listOf<Int>( 10, 20, 100, 5)
+    val isAny = nums.any()
+    val isAnyOdd = nums.any{ (it % 1) > 0 }
+    val isAnyBig = nums.any{ it > 1000 }
+
+
+    ///“Another boolean function, all { predicate }, returns true only if every element in the list matches the predicate:”
+    val isAnyAll = nums.all { it % 1 > 0 } // false”
+
+    ///“The opposite of any is none. Without a predicate, none() returns true only if there are no elements in a collection. With a predicate, none { predicate } returns true only if the predicate evaluates to true for none of the elements in the collection. ”
+    val isAny4 = nums.none { it != 4 }
+
+
+
+
+    println("numbs -> $nums")
+    println("isAny -> $isAny")
+    println("isAnyOdd -> $isAnyOdd")
+    println("isAnyBig -> $isAnyBig")
+    println("isAnyAll -> $isAnyAll")
+    println("isAny4 -> $isAny4")
+
+
+
+
+
 
 }
 
