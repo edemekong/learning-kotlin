cf8b6fb7a557070e7bff94646ced5b5e37e979f2
Paul Edemekong <hello.paulvin@gmail.com>
2022-01-27 05:11:36 +0100

    first commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,13 @@
+//map-function
+
 
 fun main(args: Array<String>) {
+    val numb = listOf<Int?>(10, null, 20, 30, 40, 50, 60, 70, 80,90, 100)
+    val divideNumb = numb.mapNotNull { it?.div(2) }
 
+    val index = numb.mapIndexed{ index: Int, value: Int? -> { println(value) } }
+
+    println(divideNumb)
 }
 
 
